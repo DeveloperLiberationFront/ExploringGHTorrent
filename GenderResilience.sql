27ccdcf265a05c9bac6bf5cd3f26b10dd2b0ccac
Clarissa Rainear <clarrain@users.noreply.github.com>
2016-01-26 17:24:14 -0500

    Update GenderResilience.sql
    Added the number of records the first statement returns (21890)

--- GenderResilience.sql
+++ GenderResilience.sql
@@ -1,4 +1,5 @@
 -- finds records of the females who have closed pull requests, sorts the female and the index of their pull request in ascending order
+--returns 21890 rows
 SELECT pull_request_id, creator_id, creator_pull_index 
 FROM g_pull_closes
 WHERE gplus_gender = 'female' AND state = 'closed'
